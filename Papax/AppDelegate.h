660bd0da5f5076e3fe7864c6ef4e130823e12850
VaruzhKhachatryan <varuzhan.khachatryan@picsart.com>
2018-06-07 19:27:09 +0400

    created project

---
+++ Papax/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Papax
+//
+//  Created by VA on 6/7/18.
+//  Copyright © 2018 VA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Papax/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Papax
+//
+//  Created by VA on 6/7/18.
+//  Copyright © 2018 VA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
