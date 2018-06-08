1c80b532ad4ae8ea987ebc8fdf68c2dc48f6833e
VaruzhKhachatryan <varuzhan.khachatryan@picsart.com>
2018-06-08 18:15:41 +0400

    second page

---
+++ Papax/Registration/RegistrationCarInfo.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationCarInfo.h
+//  Papax
+//
+//  Created by VA on 6/8/18.
+//  Copyright © 2018 VA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationCarInfo : UIViewController
+
+@end
---
+++ Papax/Registration/RegistrationCarInfo.m
@@ -0,0 +1,42 @@
+//
+//  RegistrationCarInfo.m
+//  Papax
+//
+//  Created by VA on 6/8/18.
+//  Copyright © 2018 VA. All rights reserved.
+//
+
+#import "RegistrationCarInfo.h"
+
+@interface RegistrationCarInfo () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIView *avatarView;
+@property (weak, nonatomic) IBOutlet UIImageView *cameraIconImageView;
+@property (weak, nonatomic) IBOutlet UIButton *imagePickerButton;
+
+@end
+
+@implementation RegistrationCarInfo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.avatarView bringSubviewToFront:self.cameraIconImageView];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)imagePicker:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = NO;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerOriginalImage];
+    [self.imagePickerButton setImage:chosenImage forState:UIControlStateNormal];
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end
