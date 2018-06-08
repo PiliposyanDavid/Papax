3db7e45f2d61c14db0c3ce181cbad2e010ac2760
VaruzhKhachatryan <varuzhan.khachatryan@picsart.com>
2018-06-08 22:23:50 +0400

    2 screen

---
+++ Papax/Registration/RegistrationUserInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationUserInfoViewController.h
+//  Papax
+//
+//  Created by VA on 6/8/18.
+//  Copyright © 2018 VA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationUserInfoViewController : UIViewController
+
+@end
