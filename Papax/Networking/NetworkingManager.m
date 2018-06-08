8c7fe9e7e2c2f75d32524afa46a71de75f41aebb
barseghyan-tigran <tigran.barseghyan@picsart.com>
2018-06-08 11:21:38 +0400

    Added NetworkingManager.

---
+++ Papax/Networking/NetworkingManager.m
@@ -0,0 +1,74 @@
+//
+//  NetworkingManager.m
+//  Papax
+//
+//  Created by Tigran on 6/8/18.
+//  Copyright © 2018 VA. All rights reserved.
+//
+
+#import "NetworkingManager.h"
+#import <AFNetworking.h>
+
+@interface NetworkingManager ()
+
+@property (nonatomic) AFURLSessionManager *sessionManager;
+@property (nonatomic) AFHTTPSessionManager *httpSessionManager;
+
+@property (nonatomic) UIActivityIndicatorView *activityIndicatorView;
+
+@end
+
+
+@implementation NetworkingManager
+
++ (instancetype)sharedInstance
+{
+    static NetworkingManager *sharedMyManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMyManager = [[self alloc] init];
+    });
+    return sharedMyManager;
+}
+
+- (id)init {
+    if (self = [super init]) {
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        self.sessionManager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+        self.httpSessionManager = [[AFHTTPSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+        
+        self.activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+        self.activityIndicatorView.center = keyWindow.center;
+        [keyWindow addSubview:self.activityIndicatorView];
+        
+    }
+    return self;
+}
+
+- (void)requestToPath:(NSString *)path method:(NSString *)method params:(NSDictionary *)params completionHandler:(void (^)(NSURLResponse *response, id responseObject, NSError *error))completionHandler {
+    if (!path) {
+        return;
+    }
+    NSMutableURLRequest *request = [[AFJSONRequestSerializer serializer] requestWithMethod:method
+                                                                                 URLString:[BASE_URL stringByAppendingString:path]
+                                                                                parameters:params error:NULL];
+    self.activityIndicatorView.hidden = NO;
+    [self.activityIndicatorView startAnimating];
+    [UIApplication sharedApplication].keyWindow.userInteractionEnabled = NO;
+
+    [[self.sessionManager dataTaskWithRequest:request completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+
+        self.activityIndicatorView.hidden = YES;
+        [self.activityIndicatorView stopAnimating];
+        [UIApplication sharedApplication].keyWindow.userInteractionEnabled = YES;
+ 
+        if (completionHandler) {
+            completionHandler(response, responseObject, error);
+        }
+
+    }] resume];
+}
+
+
+@end
