64c5539cc3d8232625c1779007234b8f38350136
VaruzhKhachatryan <varuzhan.khachatryan@picsart.com>
2018-06-08 19:55:07 +0400

    added disable state

--- GradientButton.m
+++ GradientButton.m
@@ -29,22 +29,24 @@
 
 - (void)drawRect:(CGRect)rect {
     [super drawRect:rect];
-    if (self.darkBlueVariant) {
-        self.colors = @[(id)([UtilMethods colorFromHexString:@"#303D99"]).CGColor, (id)([UtilMethods colorFromHexString:@"#3478B9"]).CGColor];
-    } else {
-        self.colors = @[(id)([UtilMethods colorFromHexString:@"#42BFEA"]).CGColor, (id)([UtilMethods colorFromHexString:@"#77E1F6"]).CGColor];
+    if (self.enabled) {
+        if (self.darkBlueVariant) {
+            self.colors = @[(id)([UtilMethods colorFromHexString:@"#303D99"]).CGColor, (id)([UtilMethods colorFromHexString:@"#3478B9"]).CGColor];
+        } else {
+            self.colors = @[(id)([UtilMethods colorFromHexString:@"#42BFEA"]).CGColor, (id)([UtilMethods colorFromHexString:@"#77E1F6"]).CGColor];
+        }
+        
+        GradientView *gradientView = [[GradientView alloc] initWithFrame:self.bounds];
+        gradientView.userInteractionEnabled = NO;
+        gradientView.locations = @[@0, @1];
+        gradientView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        [gradientView setStartPoint:CGPointMake(CGRectGetMinX(self.bounds), CGRectGetMidY(self.bounds))
+                        andEndPoint:CGPointMake(CGRectGetMaxX(self.bounds), CGRectGetMidY(self.bounds))];
+        gradientView.colors = self.colors;
+        [self addSubview:gradientView];
+        
+        self.titleLabel.layer.zPosition = 100;
     }
-    
-    GradientView *gradientView = [[GradientView alloc] initWithFrame:self.bounds];
-    gradientView.userInteractionEnabled = NO;
-    gradientView.locations = @[@0, @1];
-    gradientView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    [gradientView setStartPoint:CGPointMake(CGRectGetMinX(self.bounds), CGRectGetMidY(self.bounds))
-                         andEndPoint:CGPointMake(CGRectGetMaxX(self.bounds), CGRectGetMidY(self.bounds))];
-    gradientView.colors = self.colors;
-    [self addSubview:gradientView];
-    
-    self.titleLabel.layer.zPosition = 100;
 }
 
 @end
